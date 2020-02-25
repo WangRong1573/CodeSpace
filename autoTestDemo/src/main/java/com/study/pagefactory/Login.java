bc460322860eeb441ea00d18cdcbbf58ab642bc2
rong.wang <345001183@qq.com>
2020-02-25 19:45:13 +0800

    page factory 模式练习

---
+++ autoTestDemo/src/main/java/com/study/pagefactory/Login.java
@@ -0,0 +1,15 @@
+package com.study.pagefactory;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author rong.wang
+ * @date 21:58  2020/2/24
+ */
+public class Login {
+    @Test
+    public void login(){
+        LoginStep loginStep=new LoginStep();
+        loginStep.step("18272691567","yucheng2017","登陆成功");
+    }
+}
---
+++ autoTestDemo/src/main/java/com/study/pagefactory/LoginStep.java
@@ -0,0 +1,24 @@
+package com.study.pagefactory;
+
+import org.testng.Assert;
+
+/**
+ * @author rong.wang
+ * @date 21:52  2020/2/24
+ * 操作层
+ */
+public class LoginStep {
+    public void step(String username,String password,String expected){
+        BasePage basePage=new BasePage();
+        basePage.open();
+        LoginPage loginPage=basePage.loginPage();
+        loginPage.close();
+        loginPage.login();
+        loginPage.sendUsername(username);
+        loginPage.sendPassword(password);
+        loginPage.login2();
+
+        String errorMsg = loginPage.getErrorMsg();
+        Assert.assertEquals(errorMsg,expected);
+    }
+}
