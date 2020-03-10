94add43f38d2f4d86048408bd10d95903b94bb0f
rong.wang <345001183@qq.com>
2020-03-10 22:05:34 +0800

    抽象类方法

---
+++ LearnTestng/src/main/java/com/java/test/American2.java
@@ -0,0 +1,11 @@
+package com.java.test;
+
+/**
+ * @author rong.wang
+ * @date 22:03  2020/3/10
+ */
+public class American2 extends Person2 {
+    void say() {
+        System.out.println("老外说英语");
+    }
+}
---
+++ LearnTestng/src/main/java/com/java/test/Person2.java
@@ -0,0 +1,9 @@
+package com.java.test;
+
+/**
+ * @author rong.wang
+ * @date 22:01  2020/3/10
+ */
+public abstract class Person2 {
+    abstract void say();
+}
