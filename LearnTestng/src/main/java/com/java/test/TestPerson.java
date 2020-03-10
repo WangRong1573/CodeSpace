89abd84b6b2f38c53093055b20ae4b6840dd728f
rong.wang <345001183@qq.com>
2020-03-10 22:01:20 +0800

    继承、多态、方法重写添加注释

--- LearnTestng/src/main/java/com/java/test/TestPerson.java
+++ LearnTestng/src/main/java/com/java/test/TestPerson.java
@@ -9,7 +9,7 @@ public class TestPerson {
         //多态之父类引用指向子类对象
         Person p1=new Chinese();
         p1.say();
-
+        //多态的使用前提是必须有继承关系
         Person p2=new American();
         p2.say();
     }
