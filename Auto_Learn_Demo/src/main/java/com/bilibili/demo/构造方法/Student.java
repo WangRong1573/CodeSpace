170319289396aa1b33853ffa97bbc4505af2651a
rong.wang <345001183@qq.com>
2020-06-02 23:24:34 +0800

    构造方法01

---
+++ Auto_Learn_Demo/src/main/java/com/bilibili/demo/构造方法/Student.java
@@ -0,0 +1,18 @@
+package com.bilibili.demo.构造方法;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:
+ * @Author rong.wang
+ * @Date 2020/6/2
+ **/
+
+public class Student {
+    String name;
+    int id;
+
+    public Student() {
+        System.out.println("无参构造方法执行");
+    }
+}
