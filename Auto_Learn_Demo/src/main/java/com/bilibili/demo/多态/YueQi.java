49edcd1291b291adb2b4b529acc6b43809483175
rong.wang <345001183@qq.com>
2020-06-16 22:46:58 +0800

    多态练习：模拟"乐者"通过"演奏"不同的"乐器"发出"不同的声音"

---
+++ Auto_Learn_Demo/src/main/java/com/bilibili/demo/多态/YueQi.java
@@ -0,0 +1,16 @@
+package com.bilibili.demo.多态;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:
+ * @Author rong.wang
+ * @Date 2020/6/16
+ **/
+
+//乐器类，父类
+public class YueQi {
+    public void makeSound(){
+
+    }
+}
