35a25e3b4e2ce9aba51022c9af4faf2e7ee721b0
rong.wang <345001183@qq.com>
2020-07-21 21:48:26 +0800

    数组的扩容

---
+++ Auto_Learn_Demo/src/main/java/com/bilibili/demo/array/ArrayTest02.java
@@ -0,0 +1,16 @@
+package com.bilibili.demo.array;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:数组的拷贝
+ * 原理：新建一个大容量的数组，将源数组数据copy一份放在大容量数组中
+ * 数组的扩容因为涉及到拷贝问题，所以效率很低，尽可能少的进行数组的拷贝
+ * 解决办法：创建对象的时候预估多少长度合适，预估准确，减少数组扩容次数，提高效率
+ * 调用方法：System.arrayCopy();
+ * @Author rong.wang
+ * @Date 2020/7/21
+ **/
+
+public class ArrayTest02 {
+}
