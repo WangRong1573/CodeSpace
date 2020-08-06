cbb502b07f105527d435139234c8146d231062e9
rong.wang <345001183@qq.com>
2020-08-06 21:41:11 +0800

    获取当前线程对象 public static Thread currentThread()

--- Auto_Learn_Demo/src/main/java/com/bilibili/demo/thread/ThreadTest04.java
+++ Auto_Learn_Demo/src/main/java/com/bilibili/demo/thread/ThreadTest04.java
@@ -4,7 +4,8 @@ package com.bilibili.demo.thread;
  * tip：好好学习，天天向上！坚持
  *
  * @Description:
- * 怎么获取当前线程对象
+ * 怎么获取  当前线程对象  public static Thread currentThread()
+ *  Thread t = Thread.currentThread()
  * 获取线程对象的名字  thread.getName()
  * 修改线程对象的名字  thread.getName()
  * @Author rong.wang
@@ -13,6 +14,11 @@ package com.bilibili.demo.thread;
 
 public class ThreadTest04 {
     public static void main(String[] args) {
+
+        //出现在main方法中，所以当前线程就是主线程
+        Thread thread2 = Thread.currentThread();
+        System.out.println(thread2.getName());//main
+
         Thread thread = new Thread(new MyRunnable2());
 
         System.out.println(thread.getName()); //Thread-0
@@ -37,7 +43,9 @@ class MyRunnable2 implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 1000; i++) {
-            System.out.println("分支线程" + i);
+            //如果是t1线程执行run方法，当前为t1;t2线程执行run方法，当前为t2
+            String name = Thread.currentThread().getName();
+            System.out.println("线程名字" +name);
         }
     }
 }
