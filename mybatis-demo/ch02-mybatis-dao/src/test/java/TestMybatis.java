3578e00df6eb2b96bc6cc27a39240080514626c8
rong.wang <345001183@qq.com>
2020-09-24 22:01:15 +0800

    初识动态代理机制

--- mybatis-demo/ch02-mybatis-dao/src/test/java/TestMybatis.java
+++ mybatis-demo/ch02-mybatis-dao/src/test/java/TestMybatis.java
@@ -17,7 +17,24 @@ public class TestMybatis {
 
     @Test
     public void testSelectStudent(){
+        //dao.StudentDao
         StudentDaoImpl dao = new StudentDaoImpl();
+        /**
+         * List<Student> students = dao.selectStudents(); 调用
+         * 1.dao对象，类型是StudentDao，全限定名称是dao.StudentDao 和studentDao.xml的namespace值是一样的
+         *
+         * 2.方法名称selectStudents(),与mapper文件studentDao.xml的id是一样的
+         *
+         * 3.通过dao中的返回值，mybatis可以确定要调用的SqlSession的方法
+         *      如果返回值是list，调用的是SqlSession.selectList()方法
+         *      如果返回值是int，或者其他类型，非list类型，根据mapper文件中的标签是<insert>,或者<update>
+         *       就会调用SqlSession的insert()或者update()方法
+         *
+         *
+         * Mybatis的动态代理：
+         *      mybatis根据dao的调用方法，获取执行sql语句的信息，根据dao接口创建出一个dao接口的实现类，并创建这个类的
+         *      对象，完成SqlSession调用方法，访问数据库
+         */
         List<Student> students = dao.selectStudents();
         for (Student student:students){
             System.out.println(student);
