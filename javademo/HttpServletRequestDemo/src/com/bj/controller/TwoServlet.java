dc5ebe0d1a18500356bbe52a61d7de0cdfe7fd20
rong.wang <345001183@qq.com>
2021-02-03 20:16:22 +0800

    Servlet：关于HTTPServletRequest接口，处理post请求参数乱码问题

--- javademo/HttpServletRequestDemo/src/com/bj/controller/TwoServlet.java
+++ javademo/HttpServletRequestDemo/src/com/bj/controller/TwoServlet.java
@@ -15,9 +15,20 @@ import java.io.IOException;
  **/
 
 public class TwoServlet extends HttpServlet {
+    /**
+     * post请求处理参数是将参数放在请求体中，是由request对象负责解码，需要使用utf-8字符集进行解码
+     * 手动设置 字符集为utf-8 可以避免参数乱码问题
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setCharacterEncoding("utf-8");
+
         String username = req.getParameter("username");
         System.out.println("post 方式参数值为："+username);
     }
