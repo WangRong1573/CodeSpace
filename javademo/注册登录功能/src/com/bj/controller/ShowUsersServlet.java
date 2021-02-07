daf0649afe789e573b1673de7ad12529096a69f2
rong.wang <345001183@qq.com>
2021-02-07 19:40:54 +0800

    添加用户功能

---
+++ javademo/注册登录功能/src/com/bj/controller/ShowUsersServlet.java
@@ -0,0 +1,54 @@
+package com.bj.controller;
+
+import com.bj.dao.UsersDao;
+import com.bj.entity.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:
+ * @Author rong.wang
+ * @Date 2021/2/4
+ **/
+
+public class ShowUsersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UsersDao dao = new UsersDao();
+        ArrayList<Users> users = dao.showUsers();
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print("<table border='2' align='center'>");
+        out.print("<tr>");
+        out.print("<td>用户编号：</td>");
+        out.print("<td>用户姓名：</td>");
+        out.print("<td>用户密码：</td>");
+        out.print("<td>用户性别：</td>");
+        out.print("<td>用户邮箱：</td>");
+        out.print("</tr>");
+        for (Users u:users){
+            out.print("<tr>");
+            out.print("<td>"+u.getUserId()+"</td>");
+            out.print("<td>"+u.getUserName()+"</td>");
+            out.print("<td>"+u.getPassword()+"</td>");
+            out.print("<td>"+u.getSex()+"</td>");
+            out.print("<td>"+u.getEmail()+"</td>");
+            out.print("</tr>");
+        }
+
+        out.print("</table>");
+        for (Users u:users) {
+
+        }
+    }
+}
--- javademo/注册登录功能/src/com/bj/dao/UsersDao.java
+++ javademo/注册登录功能/src/com/bj/dao/UsersDao.java
@@ -4,7 +4,9 @@ import com.bj.entity.Users;
 import com.bj.util.JDBCUtil;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * tip：好好学习，天天向上！坚持
@@ -16,13 +18,16 @@ import java.sql.SQLException;
 
 public class UsersDao {
     private JDBCUtil util = new JDBCUtil();
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+    private ArrayList<Users> list = new ArrayList<>();
 
     public int add(Users user){
         String sql = "insert into Users(userName,password,sex,email) values(?,?,?,?)";
         int result = 0;
 
         try {
-            PreparedStatement ps = util.creatPre(sql);
+            ps = util.creatPre(sql);
             ps.setString(1,user.getUserName());
             ps.setString(2,user.getPassword());
             ps.setString(3,user.getSex());
@@ -36,5 +41,48 @@ public class UsersDao {
             util.close();
         }
         return result;
+    }
+
+    public int delete(String userId){
+        String sql = "delete from Users where userId=?";
+        int result = 0;
+
+        try {
+            ps = util.creatPre(sql);
+            ps.setInt(1, Integer.parseInt(userId));
+
+            result = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            util.close();
+        }
+        return result;
+    }
+
+    public ArrayList<Users> showUsers(){
+        String sql = "select * from Users";
+
+        try {
+            ps = util.creatPre(sql);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()){
+                int userId = rs.getInt("userId");
+                String userName = rs.getString("userName");
+                String password = rs.getString("password");
+                String sex = rs.getString("sex");
+                String email = rs.getString("email");
+
+                Users users = new Users(userId, userName, password, sex, email);
+                list.add(users);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            util.close(rs);
+        }
+        return list;
     }
 }
