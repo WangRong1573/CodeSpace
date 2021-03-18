fcfa43ef0da70da8e5dd592fce9a85a6985addbb
rong.wang <345001183@qq.com>
2021-03-18 20:40:57 +0800

    执行查询语句，返回集合类型

--- ch01_mybatis/src/main/java/com/demo/test/Test.java
+++ ch01_mybatis/src/main/java/com/demo/test/Test.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * tip：好好学习，天天向上！坚持
@@ -32,8 +33,18 @@ public class Test {
 
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = sqlSessionFactory.openSession();
-        Student student = session.selectOne("testSelect.getOneById",1);
-        System.out.println(student);
+        //参数1：通过命名空间.id值方式定位sql语句
+        //参数2：sql语句中传递的参数
+//        Student student = session.selectOne("testSelect.getOneById",1);
+//        System.out.println(student);
+
+        //查询所有学生操作
+        List<Student> students = session.selectList("testSelect.getAll");
+        for (Student student:students){
+            System.out.println(student);
+        }
+        //记得关闭
+        session.close();
     }
 
 }
