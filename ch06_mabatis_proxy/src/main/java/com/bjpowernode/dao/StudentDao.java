b7ac08e05c8bc30637a0afec53f2812a0798bc9b
rong.wang <345001183@qq.com>
2021-03-21 14:59:34 +0800

    多个参数：@Param命名参数

--- ch06_mabatis_proxy/src/main/java/com/bjpowernode/dao/StudentDao.java
+++ ch06_mabatis_proxy/src/main/java/com/bjpowernode/dao/StudentDao.java
@@ -1,6 +1,7 @@
 package com.bjpowernode.dao;
 
 import com.bjpowernode.domain.Student;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -8,4 +9,7 @@ public interface StudentDao {
     List<Student> selectStudents();
 
     int insertStudent(Student student);
+
+    List<Student> selectMultiParam(@Param("name") String name,
+                                   @Param("age") Integer age);
 }
