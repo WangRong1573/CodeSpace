9843637073ff9024fb2786d972c628691a724d2d
rong.wang <345001183@qq.com>
2021-03-24 20:46:05 +0800

    引用类型的设值注入

---
+++ spring-course/ch02-hello-spring/src/test/java/com/bjpowernode/TestStudentAndSchool.java
@@ -0,0 +1,24 @@
+package com.bjpowernode;
+
+import com.bjpowernode.ba02.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:
+ * @Author rong.wang
+ * @Date 2021/3/24
+ **/
+
+public class TestStudentAndSchool {
+    @Test
+    public void test02(){
+        String config = "ba02/ApplicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        Student student = (Student) ac.getBean("student");
+        System.out.println(student);
+    }
+}
