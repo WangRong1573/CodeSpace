5aad7f55e7dc95cc2bff65f99adab4e4c63e5580
rong.wang <345001183@qq.com>
2021-03-30 20:57:39 +0800

    购买商品场景搭建：创建实体类

---
+++ spring-course/ch07-spring-trans/src/main/java/com/bjpowernode/domain/Goods.java
@@ -0,0 +1,51 @@
+package com.bjpowernode.domain;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:
+ * @Author rong.wang
+ * @Date 2021/3/30
+ **/
+
+public class Goods {
+    private Integer id;
+    private String name;
+    private Integer amount;
+    private Float price;
+
+    public Goods() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
---
+++ spring-course/ch07-spring-trans/src/main/java/com/bjpowernode/domain/Sale.java
@@ -0,0 +1,42 @@
+package com.bjpowernode.domain;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:
+ * @Author rong.wang
+ * @Date 2021/3/30
+ **/
+
+public class Sale {
+    private Integer id;
+    private Integer gid;
+    private Integer nums;
+
+    public Sale() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGid() {
+        return gid;
+    }
+
+    public void setGid(Integer gid) {
+        this.gid = gid;
+    }
+
+    public Integer getNums() {
+        return nums;
+    }
+
+    public void setNums(Integer nums) {
+        this.nums = nums;
+    }
+}
