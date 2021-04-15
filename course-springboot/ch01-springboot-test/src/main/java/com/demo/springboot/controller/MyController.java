58de1589ef7ee40b896fc8a4766960dc66f5b901
rong.wang <345001183@qq.com>
2021-04-15 20:11:59 +0800

    springboot项目搭建：集成jsp

--- course-springboot/ch01-springboot-test/src/main/java/com/demo/springboot/controller/MyController.java
+++ course-springboot/ch01-springboot-test/src/main/java/com/demo/springboot/controller/MyController.java
@@ -3,6 +3,7 @@ package com.demo.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * tip：好好学习，天天向上！坚持
@@ -19,5 +20,14 @@ public class MyController {
     @ResponseBody
     public String say(){
         return "hello springboot";
+    }
+
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    public ModelAndView test(){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg","hello springboot,这是jsp显示");
+        mv.setViewName("test");
+        return mv;
     }
 }
