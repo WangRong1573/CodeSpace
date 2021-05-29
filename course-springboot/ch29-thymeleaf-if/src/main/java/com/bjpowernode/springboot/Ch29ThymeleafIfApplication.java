24d9f9f9709a2b9679dc97a73ea3fbb525921759
rong.wang <345001183@qq.com>
2021-05-29 16:58:55 +0800

    springboot项目搭建：条件判断

---
+++ course-springboot/ch29-thymeleaf-if/src/main/java/com/bjpowernode/springboot/Ch29ThymeleafIfApplication.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch29ThymeleafIfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch29ThymeleafIfApplication.class, args);
+    }
+
+}
