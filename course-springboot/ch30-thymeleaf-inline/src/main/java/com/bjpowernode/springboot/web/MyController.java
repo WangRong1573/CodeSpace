5e874b597d12e3e8b49556ca5f2e66f0f922f192
rong.wang <345001183@qq.com>
2021-05-29 17:15:39 +0800

    springboot项目搭建：内敛表达式

---
+++ course-springboot/ch30-thymeleaf-inline/src/main/java/com/bjpowernode/springboot/web/MyController.java
@@ -0,0 +1,23 @@
+package com.bjpowernode.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * tip：好好学习，天天向上！坚持
+ *
+ * @Description:
+ * @Author rong.wang
+ * @Date 2021/5/29
+ **/
+
+@Controller
+public class MyController {
+
+    @RequestMapping(value = "/test")
+    public String test(Model model){
+        model.addAttribute("data","hello springboot inline");
+        return "inline";
+    }
+}
