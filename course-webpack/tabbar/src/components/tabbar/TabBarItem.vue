e8f48c7963e32cdd77216b56a8b6d992b2d01714
rong.wang <345001183@qq.com>
2021-07-12 21:01:32 +0800

    tabBar和TabBarItem的组件封装

---
+++ course-webpack/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-img"></slot>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem"
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-item img{
+  height: 24px;
+  width: 24px;
+  margin-top: 3px;
+  /*去除图片底部3个px的高度*/
+  vertical-align: middle;
+}
+</style>
