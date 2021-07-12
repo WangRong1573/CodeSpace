4fb81a01b50e5a3daaa9a8e06ab00ae9a02502f0
rong.wang <345001183@qq.com>
2021-07-12 22:36:08 +0800

    TabBar-补充文件路径起别名

--- course-webpack/tabbar/src/components/MainTabBar.vue
+++ course-webpack/tabbar/src/components/MainTabBar.vue
@@ -1,8 +1,9 @@
 <template>
   <tab-bar>
     <tab-bar-item path="/home" activeColor="green">
-      <img src="../assets/img/tabbar/home.svg" alt="" slot="item-img">
-      <img src="../assets/img/tabbar/home_active.svg" alt="" slot="item-img-active">
+<!--      属性中的路径不可以使用别名来直接表示，会报错，需要使用 ~ 开头 -->
+      <img src="~assets/img/tabbar/home.svg" alt="" slot="item-img">
+      <img src="~assets/img/tabbar/home_active.svg" alt="" slot="item-img-active">
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category" activeColor="green">
@@ -24,8 +25,9 @@
 </template>
 
 <script>
-import TabBarItem from "./tabbar/TabBarItem";
-import TabBar from "./tabbar/TabBar";
+//起别名后可以直接使用别名开头
+import TabBarItem from "components/tabbar/TabBarItem";
+import TabBar from "components/tabbar/TabBar";
 
 export default {
   name: "MainTabBar",
