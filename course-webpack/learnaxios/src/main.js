3b175a72b7418b67a72cce63f4a8c73db3baa82f
rong.wang <345001183@qq.com>
2021-07-14 21:11:22 +0800

    axios get处理并发请求

--- course-webpack/learnaxios/src/main.js
+++ course-webpack/learnaxios/src/main.js
@@ -11,19 +11,59 @@ new Vue({
 
 axios({
   //默认get请求
-  url:'http://123.207.32.32:8000/home/multidata',
+  url: 'http://123.207.32.32:8000/home/multidata',
   //method:'post' ,可以发送post请求
-}).then(res =>{
+}).then(res => {
   console.log(res);
 })
 
 //带参数的get请求
 axios({
-  url:'http://123.207.32.32:8000/home/data',
-  params:{
-    type:'pop',
-    page:1
+  url: 'http://123.207.32.32:8000/home/data',
+  params: {
+    type: 'pop',
+    page: 1
   }
-}).then(res =>{
+}).then(res => {
   console.log(res);
 })
+
+//发送并发请求
+axios([axios({
+  url: 'http://123.207.32.32:8000/home/multidata'
+}), axios({
+  url: 'http://123.207.32.32:8000/home/data',
+  params: {
+    type: 'sell',
+    page: 3
+  }
+})]).then(results => {
+  console.log(results);
+  // console.log(results[0]);
+  // console.log(results[1]);
+})
+
+//并发请求的结果处理另一种方式
+axios([axios({
+  url: 'http://123.207.32.32:8000/home/multidata'
+}), axios({
+  url: 'http://123.207.32.32:8000/home/data',
+  params: {
+    type: 'sell',
+    page: 3
+  }
+})]).then(axios.spread((res1,res2)=>{
+  console.log(res1);
+  console.log(res2);
+}))
+
+//对象的解构
+const obj = {
+  name:'zs',
+  age:18,
+  height:1.88
+}
+
+const {name,age,height} = obj;
+
+
