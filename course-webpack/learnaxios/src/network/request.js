38d704c8474fac56254665bbc7e67dc86b89558d
rong.wang <345001183@qq.com>
2021-07-14 22:21:17 +0800

    axios 网络请求封装--纠正写法错误

--- course-webpack/learnaxios/src/network/request.js
+++ course-webpack/learnaxios/src/network/request.js
@@ -36,7 +36,6 @@ export function request2(config) {
 }
 
 export function request3(config) {
-  return new Promise(((resolve, reject) => {
     const instance3 = axios.create({
       baseURL: 'http://123.207.32.32:8000',
       timeout: 5000
@@ -44,6 +43,4 @@ export function request3(config) {
 
     //因为本身返回了类型就是一个Promise
     return instance3(config)
-
-  }))
 }
